1a08845866d365007dbbdf037a336b51a24f712e
Daniel Jaouen <daniel@danieljaouendevelopment.com>
2023-07-28 22:07:49 -0400

    Fix bug where server prematurely halts

--- hello_world.exs
+++ hello_world.exs
@@ -20,3 +20,4 @@ require Logger
 webserver = {Plug.Cowboy, plug: MyPlug, scheme: :http, options: [port: 4000]}
 {:ok, _} = Supervisor.start_link([webserver], strategy: :one_for_one)
 Logger.info("Plug now running on localhost:4000")
+Process.sleep(:infinity)
